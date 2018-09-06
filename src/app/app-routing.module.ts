d57ea589d2442885df394e2735ce1311456ed1d1
amityahav90 <33030590+amityahav90@users.noreply.github.com>
2018-09-06 14:11:05 +0300

    Complete app after deployment.

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,21 @@
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+import { AuthModule } from './auth/auth.module';
+
+import { PostListComponent } from './posts/post-list/post-list.component';
+import { PostCreateComponent } from './posts/post-create/post-create.component';
+import { AuthGuard } from './auth/auth.guard';
+
+const appRoutes: Routes = [
+  { path: '', component: PostListComponent },
+  { path: 'create', component: PostCreateComponent, canActivate: [AuthGuard] },
+  { path: 'edit/:id', component: PostCreateComponent, canActivate: [AuthGuard] },
+  { path: 'auth', loadChildren: './auth/auth.module#AuthModule' } // Lazy loading //
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(appRoutes)],
+  exports: [RouterModule],
+  providers: [AuthGuard]
+})
+export class AppRoutingModule {}
---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,4 @@
+export const environment = {
+  production: true,
+  apiUrl: 'http://mymessagesapp-env.bdbyz49vsu.us-east-2.elasticbeanstalk.com/api'
+};
